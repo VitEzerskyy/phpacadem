7d53bd238097d9c16dab5da58b8c2153b1b6130e
VitEzerskyy <_ezerskyy@ukr.net>
2016-11-25 10:19:57 +0200

    upd.homework_3

--- PHP/homework_3/Task11.php
+++ PHP/homework_3/Task11.php
@@ -1,17 +1,19 @@
 <?php
 function uppercase($str){
-
     $str = str_replace(". ", ".", $str);
     $arr = explode(".", $str);
     $count = count($arr);
 
-        for ($i = 0; $i < $count; $i ++) {
-            $arr[$i][0] = mb_strtoupper($arr[$i][0], 'UTF-8');
-        }
+    for ($i = 0; $i < $count; $i++) {
+        $str0 = mb_strtoupper(mb_substr($arr[$i], 0, 1));
+        $str1 = mb_substr($arr[$i], 1);
+        $arr[$i] = $str0.$str1;
+    }
+
 
     $result_str = implode(". ", $arr);
     echo $result_str;
 
 }
-uppercase("some english. text bla bla bla. kuku. yabadabadu");
+uppercase("а васька слушает да ест. а воз и ныне там. а вы друзья как ни садитесь, все в музыканты не годитесь. а король-то — голый. а ларчик просто открывался.а там хоть трава не расти");
 ?>
